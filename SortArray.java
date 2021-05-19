124a9da7121e78c5daad721722f47f17272af6ee
denisek0216 <denisek0216@gmail.com>
2021-05-19 10:37:24 -0700

    Main structure created

---
+++ SortArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SortArray {
+
+    // Method 1: Sorting integers
+    
+
+    // Method 2: Sorting strings
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ TestSortArray.java
@@ -0,0 +1,13 @@
+import jdk.jfr.Timestamp;
+
+public class TestSortArray {
+    SortArray obj = new SortArray();
+
+    public void testIntegerSort() {
+
+    }
+
+    public void testStringSort() {
+        
+    }
+}
